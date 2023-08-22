f711fb37e4f1618c9cd9846d7b4bf6fb19b37241
it-2001 <danikantos@gmail.com>
2023-08-22 22:52:26 +0200

    unix

--- ruda_build.sh
+++ ruda_build.sh
@@ -21,82 +21,88 @@
 #
 # To access the compiler and VM from anywhere, add the bin folder to your PATH
 # IMPORTANT: Ruda needs to be able to find these files to run
-#            In your system variables, add RUDA_PATH and set it to the build folder
-#
-# This script is distributed with test.rdbin
-# To test if everything works, run: rudavm test.rdbin -- "Hello World!"
-#                                  This should print "Hello World!"
-#                                  If it doesn't, check your PATH and RUDA_PATH
+#            In your shell configuration, add RUDA_PATH and set it to the build folder
 
 # user defined variables (change to your liking)
-root_dir="$(pwd)/build/" # where to put Ruda
-current_dir="$(pwd)/"   # current directory (don't change this)
-source_libs="${current_dir}ruda_std" # where to find the stdlib source code
-
-# Set RUDA_PATH environment variable
-export RUDA_PATH="${root_dir}"
+root_dir=$(pwd)/build/       # where to put Ruda
+current_dir=$(pwd)/          # current directory (don't change this)
+source_libs="${current_dir}stdlib" # where to find the stdlib source code
 
 # create bin folder
-mkdir -p "${root_dir}bin"
+mkdir -p "${root_dir}/bin"
 
 # create stdlib folder
-mkdir -p "${root_dir}stdlib"
+mkdir -p "${root_dir}/stdlib"
 
 # build vm
-if ! (cd "${current_dir}rusty-vm" && cargo build --release); then
+cd "${current_dir}vm" && cargo build --release
+if [ $? -ne 0 ]; then
     echo "Build failed"
     exit 1
 fi
 
 # build compiler
-if ! (cd "${current_dir}Rusty-compiler" && cargo build --release); then
+cd "${current_dir}compiler" && cargo build --release
+if [ $? -ne 0 ]; then
     echo "Build failed"
     exit 1
 fi
 
-path_to_vm="${current_dir}rusty-vm/target/release/rusty_vm"
-path_to_compiler="${current_dir}Rusty-compiler/target/release/rusty_danda"
-path_to_binaries="${root_dir}bin"
+path_to_vm="${current_dir}vm/target/release/rusty_vm"
+path_to_compiler="${current_dir}compiler/target/release/rusty_danda"
+path_to_binaries="${root_dir}/bin"
 
-# remove old vm and compiler
-rm -f "${root_dir}bin/rudavm" "${root_dir}bin/rudac"
+# remove old vm
+rm -f "${root_dir}/bin/rudavm"
 
-# copy vm and compiler
-cp "${path_to_vm}" "${path_to_binaries}/rudavm"
-cp "${path_to_compiler}" "${path_to_binaries}/rudac"
+# remove old compiler
+rm -f "${root_dir}/bin/rudac"
+
+# copy vm
+cp "${current_dir}vm/target/release/rusty_vm" "${root_dir}/bin/rudavm"
+
+# copy compiler
+cp "${current_dir}compiler/target/release/rusty_danda" "${root_dir}/bin/rudac"
+
+vm_rename="rudavm"
+compiler_rename="rudac"
+
+# rename vm
+mv "${root_dir}/bin/rusty_vm" "${root_dir}/bin/${vm_rename}"
+
+# rename compiler
+mv "${root_dir}/bin/rusty_danda" "${root_dir}/bin/${compiler_rename}"
 
 # copy stdlib
-lib_bin="${root_dir}stdlib"
+lib_bin="${root_dir}/stdlib"
 
 # get all folders excluding .git
-folders=( "${source_libs}"/*/ )
+folders=( $(find "${source_libs}" -mindepth 1 -maxdepth 1 -type d ! -name ".git" -exec basename {} \;) )
+
 for folder in "${folders[@]}"; do
-    folder="$(basename "${folder}")"
     # rebuild library
     echo "Building ${folder}"
-    if ! (cd "${source_libs}/${folder}" && cargo build --release); then
-        echo "Library build failed: ${folder}"
-        exit 1
-    fi
+    cd "${source_libs}/${folder}" && cargo build --release
 
-    path_to_bin="${path_to_binaries}/${folder}/target/release/lib${folder}.so"
+    echo "Path to bin: ${path_to_binaries}"
+    path_to_bin="${path_to_binaries}/${folder}/target/release/${folder}.so"
 
+    echo "Removing old ${folder}"
     # remove old stdlib
     rm -f "${lib_bin}/${folder}.so"
 
+    echo "Copying new ${folder}"
     # copy new stdlib
-    cp "${source_libs}/${folder}/target/release/lib${folder}.so" "${lib_bin}"
-
-    # rename stdlib file without 'lib' prefix
-    mv "${lib_bin}/lib${folder}.so" "${lib_bin}/${folder}.so"
+    cp "${source_libs}/${folder}/target/release/${folder}.so" "${lib_bin}"
 done
 
-path_to_ast="${current_dir}Rusty-compiler/ast"
+path_to_ast="${current_dir}compiler/ast"
 ruda_ast="${path_to_ast}/ruda.ast"
 registry_ast="${path_to_ast}/registry.ast"
 
 # remove old ast
-rm -f "${root_dir}/ruda.ast" "${root_dir}/registry.ast"
+rm -f "${root_dir}/ruda.ast"
+rm -f "${root_dir}/registry.ast"
 
 # copy new ast
 cp "${ruda_ast}" "${root_dir}"
