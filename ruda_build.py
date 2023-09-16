2c971664a9cebf34f3535bac47699a2d6609f261
it-2001 <it2001@sspu-opava.cz>
2023-09-16 21:50:05 +0200

    cross platform build script

--- ruda_build.py
+++ ruda_build.py
@@ -1,150 +1,137 @@
-# This script builds the Ruda compiler and VM and copies them to the build folder
-#
-# Prerequisites:
-# - Rust (https://www.rust-lang.org/tools/install)
-#
-# After running this script check the build folder
-# It should look something like this:
-# - bin
-#   - rudac.exe             (compiler)
-#   - rudavm.exe            (vm)
-#   - ruda.exe              (package manager)
-# - stdlib
-#   - io.dll
-#   - string.dll
-#   - ...
-# - ruda.ast
-# - registry.ast
-# - Ruda.toml               (package manager config)
-#
-# Those are the files needed to run Ruda programs
-#
-# To access the compiler and VM from anywhere add the bin folder to your PATH
-# IMPORTANT: Ruda needs to be able to find these files to run
-#            In your system variables add RUDA_PATH and set it to the build folder
-#
-# This script is distributed with test.rdbin
-# To test if everything works run: rudavm test.rdbin -- "Hello World!"
-#                                  This should print "Hello World!"
-#                                  If it doesn't check your PATH and RUDA_PATH
-#
-
-
 import os
+import shutil
+import platform
 
-import ctypes, sys
+# User-defined variables (change to your liking)
+root_dir = os.path.join(os.getcwd(), "build")  # Where to put Ruda
+current_dir = os.getcwd()  # Current directory (don't change this)
+source_libs = os.path.join(current_dir, "stdlib")  # Where to find the stdlib source code
 
+# Create bin folder
+bin_dir = os.path.join(root_dir, "bin")
+os.makedirs(bin_dir, exist_ok=True)
 
-# user defined variables (change to your liking)
-root_dir = os.path.join(os.getcwd(), "build\\") # where to put Ruda
-current_dir = os.getcwd() + "\\" # current directory (don't change this)
-source_libs = os.path.join(current_dir, "stdlib") # where to find the stdlib source code
+# Create stdlib folder
+stdlib_dir = os.path.join(root_dir, "stdlib")
+os.makedirs(stdlib_dir, exist_ok=True)
 
-# create bin folder
-if not os.path.exists(root_dir + "\\bin"):
-    os.makedirs(root_dir + "\\bin")
+# Build VM
+vm_dir = os.path.join(current_dir, "vm")
+os.chdir(vm_dir)
+os.system("cargo build --release")
 
-# create stdlib folder
-if not os.path.exists(root_dir + "\\stdlib"):
-    os.makedirs(root_dir + "\\stdlib")
+# Build compiler
+compiler_dir = os.path.join(current_dir, "compiler")
+os.chdir(compiler_dir)
+os.system("cargo build --release")
 
+# Build package manager
+pacman_dir = os.path.join(current_dir, "pacman")
+os.chdir(pacman_dir)
+os.system("cargo build --release")
 
-# build vm
-proc = os.system("cd " + current_dir + "vm && cargo build --release")
-if proc != 0:
-    print("Build failed")
-    exit(1)
+# Paths to executables and binaries
+path_to_vm = os.path.join(vm_dir, "target", "release", "rusty_vm")
+path_to_compiler = os.path.join(compiler_dir, "target", "release", "rusty_danda")
+path_to_binaries = bin_dir
+path_to_package_manager = os.path.join(pacman_dir, "target", "release", "ruda")
 
+# Determine the file extension based on the platform
+if platform.system() == "Windows":
+    executable_ext = ".exe"
+else:
+    executable_ext = ""
 
-# build compiler
-proc = os.system("cd " + current_dir + "compiler && cargo build --release")
-if proc != 0:
-    print("Build failed")
-    exit(1)
+# Check if VM binary exists
+if not os.path.exists(path_to_vm + executable_ext):
+    print("Error: VM binary not found at", path_to_vm + executable_ext)
+else:
+    # Remove old VM
+    old_vm_path = os.path.join(bin_dir, "rudavm" + executable_ext)
+    if os.path.exists(old_vm_path):
+        os.remove(old_vm_path)
 
+    # Copy VM
+    shutil.copy(path_to_vm + executable_ext, os.path.join(bin_dir, "rudavm" + executable_ext))
 
-# build package manager
-proc = os.system("cd " + current_dir + "pacman && cargo build --release")
-if proc != 0:
-    print("Build failed")
-    exit(1)
+# Check if compiler binary exists
+if not os.path.exists(path_to_compiler + executable_ext):
+    print("Error: Compiler binary not found at", path_to_compiler + executable_ext)
+else:
+    # Remove old compiler
+    old_compiler_path = os.path.join(bin_dir, "rudac" + executable_ext)
+    if os.path.exists(old_compiler_path):
+        os.remove(old_compiler_path)
 
-path_to_vm = os.path.join(current_dir, "vm\\target\\release\\rusty_vm.exe")
-path_to_compiler = os.path.join(current_dir, "compiler\\target\\release\\rusty_danda.exe")
-path_to_binaries = os.path.join(root_dir + "\\bin")
-path_to_package_manager = os.path.join(current_dir, "pacman\\target\\release\\ruda.exe")
+    # Copy compiler
+    shutil.copy(path_to_compiler + executable_ext, os.path.join(bin_dir, "rudac" + executable_ext))
 
+# Check if package manager binary exists
+if not os.path.exists(path_to_package_manager + executable_ext):
+    print("Error: Package manager binary not found at", path_to_package_manager + executable_ext)
+else:
+    # Remove old package manager
+    old_package_manager_path = os.path.join(bin_dir, "ruda" + executable_ext)
+    if os.path.exists(old_package_manager_path):
+        os.remove(old_package_manager_path)
 
-# remove old vm
-print('del ' + root_dir +'\\bin\\rusty_vm.exe')
-print('del ' + root_dir +'\\bin\\rudavm.exe')
-proc = os.system('del ' + root_dir +'\\bin\\rudavm.exe')
+    # Copy package manager
+    shutil.copy(path_to_package_manager + executable_ext, os.path.join(bin_dir, "ruda" + executable_ext))
 
-# remove old compiler
-proc = os.system('del ' + root_dir +'\\bin\\rudac.exe')
+# Define a dictionary to map file extensions for different platforms
+platform_extensions = {
+    "Windows": ".dll",
+    "Linux": ".so",
+    "Darwin": ".dylib"  # If targeting macOS
+}
 
-# remove old package manager
-proc = os.system('del ' + root_dir +'\\bin\\ruda.exe')
-
-
-# copy vm
-print('copy ' + current_dir + 'vm\\target\\release\\rusty_vm.exe ' + root_dir +'\\bin')
-proc = os.system('copy ' + current_dir + 'vm\\target\\release\\rusty_vm.exe ' + root_dir +'\\bin')
-
-# copy compiler
-proc = os.system('copy ' + current_dir + 'compiler\\target\\release\\rusty_danda.exe ' + root_dir +'\\bin')
-
-# copy package manager
-proc = os.system('copy ' + current_dir + 'pacman\\target\\release\\ruda.exe ' + root_dir +'\\bin')
-
-vm_rename = "rudavm.exe"
-compiler_rename = "rudac.exe"
-
-# rename vm
-proc = os.system('rename "' + root_dir +'\\bin\\rusty_vm.exe" ' + vm_rename)
-
-# rename compiler
-proc = os.system('rename "' + root_dir +'\\bin\\rusty_danda.exe" ' + compiler_rename)
-
-
-# copy stdlib
-lib_bin = root_dir + "\\stdlib"
-
-# get all folders excluding .git
-folders = [f for f in os.listdir(source_libs) if os.path.isdir(os.path.join(source_libs, f)) and f != ".git"]
-
-for folder in folders:
-    # /base excluded
-    if folder == "base":
-        continue
-    # rebuild library
-    print("Building " + folder)
-    proc = os.system("cd " + os.path.join(source_libs, folder) + " && cargo build --release")
-
-    print("Path to bin: " + path_to_binaries)
-    path_to_bin = os.path.join(path_to_binaries, folder, "target\\release", folder + ".dll")
-
-    print("Removing old " + folder)
-    # remove old stdlib
-    proc = os.system('del ' + lib_bin + "\\" + folder + ".dll")
-
-    print("Copying new " + folder)
-    # copy new stdlib
-    proc = os.system('copy "' + os.path.join(source_libs, folder, "target\\release", folder + ".dll") + '" "' + lib_bin + '"')
+# Determine the file extension based on the platform
+executable_ext = platform_extensions.get(platform.system(), "")
 
 
-path_to_ast = os.path.join(current_dir, "compiler\\ast")
-ruda_ast = path_to_ast + "\\ruda.ast"
-registry_ast = path_to_ast + "\\registry.ast"
+stdlib_binaries = {
+}
 
-# remove old ast
-proc = os.system('del ' + root_dir + '\\ruda.ast')
-proc = os.system('del ' + root_dir + '\\registry.ast')
+# Copy stdlib excluding "base"
+for folder in os.listdir(source_libs):
+    if folder != "base" and os.path.isdir(os.path.join(source_libs, folder)):
+        # Rebuild library
+        print("Building " + folder)
+        folder_path = os.path.join(source_libs, folder)
+        os.chdir(folder_path)
+        os.system("cargo build --release")
 
-# copy new ast
-proc = os.system('copy ' + ruda_ast + ' ' + root_dir + '')
-proc = os.system('copy ' + registry_ast + ' ' + root_dir + '')
+        # Specify the binary name for this library
+        binary_name = stdlib_binaries.get(folder, folder + executable_ext)
 
-# copy Ruda.toml if it isn't already there
-if not os.path.exists(root_dir + "\\Ruda.toml"):
-    proc = os.system('copy ' + current_dir + '\\pacman\\templates\\Ruda.toml ' + root_dir + '')
+        print("Removing old " + binary_name)
+        # Remove old stdlib
+        old_stdlib_path = os.path.join(stdlib_dir, binary_name)
+        if os.path.exists(old_stdlib_path):
+            os.remove(old_stdlib_path)
+
+        print("Copying new " + binary_name)
+        # Copy new stdlib
+        shutil.copy(os.path.join(folder_path, "target", "release", binary_name), stdlib_dir)
+
+# Copy AST files
+path_to_ast = os.path.join(current_dir, "compiler", "ast")
+ruda_ast = os.path.join(path_to_ast, "ruda.ast")
+registry_ast = os.path.join(path_to_ast, "registry.ast")
+
+# Remove old AST files
+old_ruda_ast_path = os.path.join(root_dir, "ruda.ast")
+if os.path.exists(old_ruda_ast_path):
+    os.remove(old_ruda_ast_path)
+
+old_registry_ast_path = os.path.join(root_dir, "registry.ast")
+if os.path.exists(old_registry_ast_path):
+    os.remove(old_registry_ast_path)
+
+# Copy new AST files
+shutil.copy(ruda_ast, root_dir)
+shutil.copy(registry_ast, root_dir)
+
+# Copy Ruda.toml if it isn't already there
+if not os.path.exists(os.path.join(root_dir, "Ruda.toml")):
+    shutil.copy(os.path.join(pacman_dir, "templates", "Ruda.toml"), root_dir)
